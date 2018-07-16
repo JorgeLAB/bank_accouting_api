0bc51dd7daab35a48ac750f7387592c8fa8e194c
Daniel Moreto <dfmoreto@gmail.com>
2018-07-16 02:10:55 -0300

    Add method #balane at Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,11 @@
 class Account < ApplicationRecord
   has_many :credits
   has_many :debits
-  
+
   validates :number, presence: true, uniqueness: { case_sensitive: false }, numericality: { only_integer: true }
+
+
+  def balance
+    self.credits.sum(:amount) - self.debits.sum(:amount)
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -4,7 +4,14 @@ RSpec.describe Account, type: :model do
   subject { create :account }
   it { is_expected.to have_many :debits }
   it { is_expected.to have_many :credits }
+
   it { is_expected.to validate_presence_of :number }
   it { is_expected.to validate_uniqueness_of(:number).case_insensitive }
   it { is_expected.to validate_numericality_of(:number).only_integer }
+
+  it "#balance returns credits less debits" do
+    credits = create_list(:credit, 5, account: subject, amount: 1000)
+    debits = create_list(:debit, 3, account: subject, amount: 500)
+    expect(subject.balance).to eq 3500
+  end
 end
